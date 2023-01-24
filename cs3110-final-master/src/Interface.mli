592d9e4d0ead1d57a62177d1ca212213d1604c24
ehuang2467 <123487761+ehuang2467@users.noreply.github.com>
2023-01-24 11:15:31 -0500

    Delete cs3110-final-master directory

--- cs3110-final-master/src/Interface.mli
+++
@@ -1,4 +0,0 @@
-(** Interface that allows the representation of State using visuals.*)
-
-val print_board : State.t -> unit
-(**[print_board] uses ascii characters to display the board for the user*)
